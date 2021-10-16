94d5d40b7a0edb3367fb54b410b96449d70f1eb5
omshri22121999 <omshriprasath@gmail.com>
2021-10-16 17:57:05 +0530

    INFRA: Initialize Unit Test Project

---
+++ SCMS.Portal.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+
+namespace SCMS.Portal.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void Test1() =>
+            Assert.True(true);
+    }
+}
