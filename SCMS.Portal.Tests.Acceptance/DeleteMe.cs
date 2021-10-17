8044d2bc68bbd52276bfcfc4bce084ad47aa247a
omshri22121999 <omshriprasath@gmail.com>
2021-10-17 13:37:37 +0530

    INFRA: Initiatlize Acceptance Test Project

---
+++ SCMS.Portal.Tests.Acceptance/DeleteMe.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SCMS.Portal.Tests.Acceptance
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
