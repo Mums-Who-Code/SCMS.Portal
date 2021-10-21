4dc296aa9e0dfae4c697afedd40778fb3887732f
omshri22121999 <omshriprasath@gmail.com>
2021-10-21 12:29:17 +0530

    INFRA: Merged main and added headers

--- SCMS.Portal.Tests.Unit/Services/DeleteMe.cs
+++ SCMS.Portal.Tests.Unit/Services/DeleteMe.cs
@@ -1,3 +1,7 @@
+//-----------------------------------------------------------------------
+//Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+//-----------------------------------------------------------------------
+
 using System;
 using Xunit;
 
