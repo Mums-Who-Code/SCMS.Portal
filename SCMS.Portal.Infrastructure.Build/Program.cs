f32fba11186ea23ccde53cb142201278aa3c83e7
omshri22121999 <omshriprasath@gmail.com>
2021-10-24 23:36:54 +0530

    INFRA: Added build project

---
+++ SCMS.Portal.Infrastructure.Build/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace SCMS.Console.Infrastructure.Build
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var aDotNetClient = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = ".Net",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "main" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "main" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.Windows2019,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV2
+                            {
+                                Name = "Check out"
+                            },
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name = "Setup .Net",
+
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "6.0.0-rc.2.21480.5",
+                                    IncludePrerelease = true
+                                },
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restore"
+                            },
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Build"
+                            },
+
+                            new TestTask
+                            {
+                                Name = "Test"
+                            }
+                        }
+                    }
+                }
+            };
+
+            aDotNetClient.SerializeAndWriteToFile(githubPipeline, "../../../../.github/workflows/dotnet.yml");
+        }
+    }
+}
