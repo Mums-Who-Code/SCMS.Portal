9a1d2bdd9cabb44eb2f458e255231e7a9b0c9ce6
omshri22121999 <omshriprasath@gmail.com>
2021-10-25 18:49:54 +0530

    BROKERS: Added Navigation Brokers

---
+++ SCMS.Portal.Web/Brokers/Navigation/INavigationBroker.cs
@@ -0,0 +1,11 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+namespace SCMS.Portal.Web.Brokers.Navigation
+{
+    public interface INavigationBroker
+    {
+        void NavigateTo(string route);    
+    }
+}
