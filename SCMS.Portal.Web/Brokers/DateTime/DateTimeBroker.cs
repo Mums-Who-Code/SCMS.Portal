1dff65291e71833f3d35b6cd4cf053d560892660
omshri22121999 <omshriprasath@gmail.com>
2021-10-25 18:29:57 +0530

    BROKERS: Added DateTime Broker

---
+++ SCMS.Portal.Web/Brokers/DateTime/DateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace SCMS.Portal.Web.Brokers.DateTime
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTime() =>
+            DateTimeOffset.UtcNow;
+    }
+}
---
+++ SCMS.Portal.Web/Brokers/DateTime/IDateTimeBroker.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace SCMS.Portal.Web.Brokers.DateTime
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTime();
+    }
+}
