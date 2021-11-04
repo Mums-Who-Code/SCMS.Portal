c4705d9aea633d5db0b9e1c65f92b0d6ec443282
omshri22121999 <omshriprasath@gmail.com>
2021-11-04 12:31:29 +0530

    BROKERS: Added POST for Student

--- SCMS.Portal.Web/Brokers/Apis/IApiBroker.Students.cs
+++ SCMS.Portal.Web/Brokers/Apis/IApiBroker.Students.cs
@@ -2,10 +2,13 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using SCMS.Portal.Web.Models.Foundations.Students;
+
 namespace SCMS.Portal.Web.Brokers.Apis
 {
     public partial interface IApiBroker
     {
-
+        ValueTask<Student> PostStudentAsync(Student student);
     }
 }
