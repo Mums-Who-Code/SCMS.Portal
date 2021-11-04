70ceaa09193240a642e4b4e536d252e26274380a
omshri22121999 <omshriprasath@gmail.com>
2021-11-04 10:44:48 +0530

    FOUNDATIONS: Added Student

---
+++ SCMS.Portal.Web/Models/Foundations/Students/Student.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace SCMS.Portal.Web.Models.Foundations.Students
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public StudentStatus Status { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdateDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+    }
+}
