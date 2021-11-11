580f20226ebfb4568fc33039baeca0e890c67108
omshri22121999 <omshriprasath@gmail.com>
2021-11-11 17:57:25 +0530

    FOUNDATIONS: ShouldAddStudentAsync() -> FAIL

---
+++ SCMS.Portal.Tests.Unit/Services/Foundations/Students/StudentServiceTests.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using Moq;
+using SCMS.Portal.Web.Brokers.Apis;
+using SCMS.Portal.Web.Brokers.Loggings;
+using SCMS.Portal.Web.Models.Foundations.Students;
+using SCMS.Portal.Web.Services.Foundations.Students;
+using Tynamix.ObjectFiller;
+
+namespace SCMS.Portal.Tests.Unit.Services.Foundations.Students
+{
+    public partial class StudentServiceTests
+    {
+        private readonly Mock<IApiBroker> apiBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IStudentService studentService;
+
+        public StudentServiceTests()
+        {
+            this.apiBrokerMock = new Mock<IApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.studentService = new StudentService(
+                apiBroker: apiBrokerMock.Object,
+                loggingBroker: loggingBrokerMock.Object);
+        }
+
+        private static Student CreateRandomStudent() =>
+            CreateStudentFiller().Create();
+
+        private static Filler<Student> CreateStudentFiller()
+        {
+            var filler = new Filler<Student>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(DateTimeOffset.UtcNow);
+
+            return filler;
+        }
+    }
+}
---
+++ SCMS.Portal.Web/Services/Foundations/Students/StudentService.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using SCMS.Portal.Web.Brokers.Apis;
+using SCMS.Portal.Web.Brokers.Loggings;
+using SCMS.Portal.Web.Models.Foundations.Students;
+
+namespace SCMS.Portal.Web.Services.Foundations.Students
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IApiBroker apiBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public StudentService(
+            IApiBroker apiBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.apiBroker = apiBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Student> AddStudentAsyc(Student student)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
