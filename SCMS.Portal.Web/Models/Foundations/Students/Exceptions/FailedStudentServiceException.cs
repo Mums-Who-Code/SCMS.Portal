a29730bbfaf87209d648a528a3f7ef7495193073
omshri22121999 <omshriprasath@gmail.com>
2021-11-20 15:53:29 +0530

    CODE RUB: Fixed ShouldThrowServiceExceptiononOnAddIfServiceErrorOccursAndLogItAsync

---
+++ SCMS.Portal.Web/Models/Foundations/Students/Exceptions/FailedStudentServiceException.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace SCMS.Portal.Web.Models.Foundations.Students.Exceptions
+{
+    public class FailedStudentServiceException : Xeption
+    {
+        public FailedStudentServiceException(Exception innerException)
+            : base(message: "Failed student dependency error occurred.", innerException)
+        { }
+    }
+}
