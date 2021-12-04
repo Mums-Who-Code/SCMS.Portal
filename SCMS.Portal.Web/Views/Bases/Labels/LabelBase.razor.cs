1a0ffe21691f6ab80d36b3fe400122e6fe9fb78d
Shri Humrudha <shrija@microsoft.com>
2021-12-04 14:37:43 -0800

    BASE: Label

---
+++ SCMS.Portal.Web/Views/Bases/Labels/LabelBase.razor.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace SCMS.Portal.Web.Views.Bases.Labels
+{
+    public partial class LabelBase : ComponentBase
+    {
+        [Parameter]
+        public string Value { get; set; }
+
+        [Parameter]
+        public string Color { get; set; }
+
+        public void SetValue(string value)
+        {
+            this.Value = value;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void SetColor(string color) =>
+            this.Color = color;
+    }
+}
