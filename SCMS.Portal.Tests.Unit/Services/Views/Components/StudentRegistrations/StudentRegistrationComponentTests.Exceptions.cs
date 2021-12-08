2f3e7e436bccf49c4723c69e78b23cbeb124022b
Shri Humrudha <shrija@microsoft.com>
2021-12-08 09:17:26 -0800

    ShouldRenderExceptionMessageIfRenderErrorOccurs -> FAIL

--- SCMS.Portal.Tests.Unit/Services/Views/Components/StudentRegistrations/StudentRegistrationComponentTests.Exceptions.cs
+++ SCMS.Portal.Tests.Unit/Services/Views/Components/StudentRegistrations/StudentRegistrationComponentTests.Exceptions.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System;
 using Bunit;
 using FluentAssertions;
 using Moq;
@@ -73,6 +74,51 @@ namespace SCMS.Portal.Tests.Unit.Services.Views.Components.StudentRegistrations
             this.studentViewServiceMock.Setup(service =>
                 service.AddStudentViewAsync(It.IsAny<StudentView>()))
                     .ThrowsAsync(studentViewDependencyException);
+
+            // when
+            this.renderedStudentRegistrationComponent =
+                RenderComponent<StudentRegistrationComponent>();
+
+            this.renderedStudentRegistrationComponent.Instance
+                .RegisterButton.Click();
+
+            // then
+            this.renderedStudentRegistrationComponent.Instance
+                .StatusLabel.Value.Should().Be(expectedErrorMessage);
+
+            this.renderedStudentRegistrationComponent.Instance
+                .StatusLabel.Color.Should().Be(Color.Red);
+
+            this.renderedStudentRegistrationComponent.Instance
+                .FirstNameTextBox.IsDisabled.Should().BeFalse();
+
+            this.renderedStudentRegistrationComponent.Instance
+                .LastNameTextBox.IsDisabled.Should().BeFalse();
+
+            this.renderedStudentRegistrationComponent.Instance
+                .DateOfBirthPicker.IsDisabled.Should().BeFalse();
+
+            this.renderedStudentRegistrationComponent.Instance
+                .RegisterButton.IsDisabled.Should().BeFalse();
+
+            this.studentViewServiceMock.Verify(service =>
+                service.AddStudentViewAsync(It.IsAny<StudentView>()),
+                    Times.Once);
+
+            this.studentViewServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ShouldRenderExceptionMessageIfRenderErrorOccurs()
+        {
+            // given
+            string randomMessage = GetRandomString();
+            string expectedErrorMessage = randomMessage;
+            var exception = new Exception(randomMessage);
+
+            this.studentViewServiceMock.Setup(service =>
+                service.AddStudentViewAsync(It.IsAny<StudentView>()))
+                    .ThrowsAsync(exception);
 
             // when
             this.renderedStudentRegistrationComponent =
