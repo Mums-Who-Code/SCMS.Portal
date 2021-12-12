59f5c02ef3fbfad2dbfdeb637529b87fba75051d
Shri Humrudha <shrija@microsoft.com>
2021-12-12 09:22:56 -0800

    Revert "ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync -> FAIL"
    This reverts commit e38a08d62bf5e58015e5b47501f99c968e80aa1e.

--- SCMS.Portal.Tests.Unit/Services/Views/Foundations/SchoolViews/SchoolViewServiceTests.Exceptions.RetrieveAll.cs
+++ SCMS.Portal.Tests.Unit/Services/Views/Foundations/SchoolViews/SchoolViewServiceTests.Exceptions.RetrieveAll.cs
@@ -2,7 +2,6 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
@@ -48,45 +47,6 @@ namespace SCMS.Portal.Tests.Unit.Services.Views.Foundations.SchoolViews
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedSchoolViewDependencyException))),
-                        Times.Once);
-
-            this.schoolServiceMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAndLogItAsync()
-        {
-            // given
-            var serviceException = new Exception();
-
-            var failedSchoolViewServiceException =
-                new FailedSchoolViewServiceException(
-                    serviceException);
-
-            var expectedSchoolViewServiceException =
-                new SchoolViewServiceException(
-                    failedSchoolViewServiceException);
-
-            this.schoolServiceMock.Setup(service =>
-                service.RetrieveAllSchoolsAsync())
-                    .ThrowsAsync(serviceException);
-
-            // when
-            ValueTask<List<SchoolView>> retrieveAllSchoolViewsTask =
-                this.schoolViewService.RetrieveAllSchoolsAsync();
-
-            // then
-            await Assert.ThrowsAsync<SchoolViewServiceException>(() =>
-                retrieveAllSchoolViewsTask.AsTask());
-
-            this.schoolServiceMock.Verify(service =>
-                service.RetrieveAllSchoolsAsync(),
-                    Times.Once());
-
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(
-                    expectedSchoolViewServiceException))),
                         Times.Once);
 
             this.schoolServiceMock.VerifyNoOtherCalls();
