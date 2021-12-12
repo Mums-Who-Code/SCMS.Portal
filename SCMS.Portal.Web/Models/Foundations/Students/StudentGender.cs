e56c4e3deeddf66ddebf780b25e8db0eb57ed929
Shri Humrudha <shrija@microsoft.com>
2021-12-11 20:44:54 -0800

    ShouldThrowValidationExceptionOnAddIfStudentIsInvalidAndLogItAsync -> FAIL

---
+++ SCMS.Portal.Web/Models/Foundations/Students/StudentGender.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+namespace SCMS.Portal.Web.Models.Foundations.Students
+{
+    public enum StudentGender
+    {
+        Female,
+        Male,
+        Other
+    }
+}
