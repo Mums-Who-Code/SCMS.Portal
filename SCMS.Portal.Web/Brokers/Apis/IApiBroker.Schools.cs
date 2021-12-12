b5e2828bf0ae1d070b26f10f51c7fc72a0d895db
Shri Humrudha <shrija@microsoft.com>
2021-12-11 21:49:22 -0800

    BROKER: Get All Schools

---
+++ SCMS.Portal.Web/Brokers/Apis/IApiBroker.Schools.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using SCMS.Portal.Web.Models.Foundations.Schools;
+
+namespace SCMS.Portal.Web.Brokers.Apis
+{
+    public partial interface IApiBroker
+    {
+        ValueTask<IQueryable<School>> GetAllSchoolsAsync();
+    }
+}
