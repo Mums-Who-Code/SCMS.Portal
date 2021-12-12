1588f5b59fdf7fcd22b31b2b4aa02641f4638989
Shri Humrudha <shrija@microsoft.com>
2021-12-11 22:04:14 -0800

    ShouldRetrieveAllSchoolsAsync -> FAIL

---
+++ SCMS.Portal.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using SCMS.Portal.Web.Models.Foundations.Schools;
+using Xunit;
+
+namespace SCMS.Portal.Tests.Unit.Services.Foundations.Schools
+{
+    public partial class SchoolServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveAllSchoolsAsync()
+        {
+            // given
+            IQueryable<School> randomSchools = CreateRandomSchools();
+            IQueryable<School> retrievedSchools = randomSchools;
+            IQueryable<School> expectedSchools = retrievedSchools;
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.GetAllSchoolsAsync())
+                    .ReturnsAsync(retrievedSchools);
+
+            // when
+            IQueryable<School> actualSchools =
+                await this.schoolService.RetrieveAllSchools();
+
+            // then
+            actualSchools.Should().BeEquivalentTo(expectedSchools);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.GetAllSchoolsAsync(),
+                    Times.Once());
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ SCMS.Portal.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.cs
@@ -0,0 +1,56 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Moq;
+using SCMS.Portal.Web.Brokers.Apis;
+using SCMS.Portal.Web.Brokers.Loggings;
+using SCMS.Portal.Web.Models.Foundations.Schools;
+using SCMS.Portal.Web.Services.Foundations.Schools;
+using Tynamix.ObjectFiller;
+
+namespace SCMS.Portal.Tests.Unit.Services.Foundations.Schools
+{
+    public partial class SchoolServiceTests
+    {
+        private readonly Mock<IApiBroker> apiBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ISchoolService schoolService;
+
+        public SchoolServiceTests()
+        {
+            this.apiBrokerMock = new Mock<IApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.schoolService = new SchoolService(
+                apiBroker: this.apiBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static IQueryable<School> CreateRandomSchools()
+        {
+            return CreateRandomSchoolFiller()
+                .Create(count: GetRandomNumber()).AsQueryable();
+        }
+
+        private static Filler<School> CreateRandomSchoolFiller()
+        {
+            var filler = new Filler<School>();
+            Guid randomGuid = Guid.NewGuid();
+            DateTimeOffset dates = GetRandomDateTime();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(valueToUse: dates);
+
+            return filler;
+        }
+    }
+}
