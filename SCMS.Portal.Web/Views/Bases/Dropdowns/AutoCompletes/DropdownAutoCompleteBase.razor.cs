5eba2ab8d91b07173b647810744d147f88008b46
Shri Humrudha <shrija@microsoft.com>
2021-12-15 08:37:12 -0800

    BASE: Dropdown Auto Complete

--- SCMS.Portal.Web/Views/Bases/Dropdowns/AutoCompletes/DropdownAutoCompleteBase.razor.cs
+++ SCMS.Portal.Web/Views/Bases/Dropdowns/AutoCompletes/DropdownAutoCompleteBase.razor.cs
@@ -2,37 +2,40 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Syncfusion.Blazor.DropDowns;
 
-namespace SCMS.Portal.Web.Views.Bases.Dropdowns.Constants
+namespace SCMS.Portal.Web.Views.Bases.Dropdowns.AutoCompletes
 {
-    public partial class Dropdown<TEnum> : ComponentBase
+    public partial class DropdownAutoCompleteBase<TClass> : ComponentBase
     {
         [Parameter]
-        public TEnum Value { get; set; }
-
-        [Parameter]
-        public EventCallback<TEnum> ValueChanged { get; set; }
+        public string Placeholder { get; set; }
 
         [Parameter]
         public bool IsDisabled { get; set; }
 
-        public bool IsEnabled => IsDisabled is false;
+        private bool IsEnabled => IsDisabled is false;
 
-        public IReadOnlyList<string> EnumNames => Enum.GetNames(typeof(TEnum));
+        [Parameter]
+        public List<TClass> Items { get; set; }
 
-        public async Task SetValue(TEnum value)
+        [Parameter]
+        public string Value { get; set; }
+
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+
+        public async Task SetValue(string value)
         {
             this.Value = value;
             await ValueChanged.InvokeAsync(value);
         }
 
         public async Task OnValueChanged(
-            ChangeEventArgs<TEnum, string> changeEventArgs)
+            ChangeEventArgs<string, TClass> changeEventArgs)
         {
             await SetValue(changeEventArgs.Value);
         }
