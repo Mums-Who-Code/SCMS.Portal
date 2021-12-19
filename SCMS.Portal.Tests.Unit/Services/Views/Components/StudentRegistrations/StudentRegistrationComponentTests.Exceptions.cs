246e870839348231231b567e731a42da14d9059a
omshri22121999 <omshriprasath@gmail.com>
2021-12-19 14:36:43 +0530

    ShouldRenderInnerExceptionMessageIfValidationErrorOccurs <- FAIL

--- SCMS.Portal.Tests.Unit/Services/Views/Components/StudentRegistrations/StudentRegistrationComponentTests.Exceptions.cs
+++ SCMS.Portal.Tests.Unit/Services/Views/Components/StudentRegistrations/StudentRegistrationComponentTests.Exceptions.cs
@@ -65,6 +65,12 @@ namespace SCMS.Portal.Tests.Unit.Services.Views.Components.StudentRegistrations
                 .GenderDropdown.IsDisabled.Should().BeFalse();
 
             this.renderedStudentRegistrationComponent.Instance
+                .FideIdTextBox.IsDisabled.Should().BeFalse();
+
+            this.renderedStudentRegistrationComponent.Instance
+                .NotesTextBox.IsDisabled.Should().BeFalse();
+
+            this.renderedStudentRegistrationComponent.Instance
                 .RegisterButton.IsDisabled.Should().BeFalse();
 
             this.studentViewServiceMock.Verify(service =>
