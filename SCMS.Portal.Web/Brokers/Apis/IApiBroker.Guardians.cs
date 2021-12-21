1d640476eb2db53899c5e7a644ee262a2a48c149
omshri22121999 <omshriprasath@gmail.com>
2021-12-21 10:29:17 +0530

    - Added Guardian Model - Created Guardian Api Broker - Added Post Guardian Method

---
+++ SCMS.Portal.Web/Brokers/Apis/IApiBroker.Guardians.cs
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using SCMS.Portal.Web.Models.Foundations.Guardians;
+
+namespace SCMS.Portal.Web.Brokers.Apis
+{
+    public partial interface IApiBroker
+    {
+        ValueTask<Guardian> PostGuardianAsync(Guardian guardian);
+    }
+}
