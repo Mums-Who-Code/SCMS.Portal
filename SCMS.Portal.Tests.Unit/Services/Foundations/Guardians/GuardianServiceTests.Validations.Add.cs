0665e3241d8bb17ff81dfb15052dfa456d58f3a0
omshri22121999 <omshriprasath@gmail.com>
2021-12-30 15:24:03 +0530

    ShouldThrowValidationExceptionOnAddIfGuardianIsInvalidAndLogItAsync -> FAIL

--- SCMS.Portal.Tests.Unit/Services/Foundations/Guardians/GuardianServiceTests.Validations.Add.cs
+++ SCMS.Portal.Tests.Unit/Services/Foundations/Guardians/GuardianServiceTests.Validations.Add.cs
@@ -4,6 +4,7 @@
 
 using System.Threading.Tasks;
 using Moq;
+using NuGet.Frameworks;
 using SCMS.Portal.Web.Models.Foundations.Guardians;
 using SCMS.Portal.Web.Models.Foundations.Guardians.Exceptions;
 using Xunit;
@@ -38,6 +39,90 @@ namespace SCMS.Portal.Tests.Unit.Services.Foundations.Guardians
             this.apiBrokerMock.Verify(broker =>
                 broker.PostGuardianAsync(It.IsAny<Guardian>()),
                     Times.Never());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfGuardianIsInvalidAndLogItAsync(string invalidFirstName)
+        {
+            //given
+            Guardian invalidGuardian = new Guardian
+            {
+                FirstName = invalidFirstName,
+                Title = Title.None
+            };
+
+            var invalidGuardianException = new InvalidGuardianException();
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.Id),
+                values: "Id is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.FirstName),
+                values: "Text is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.LastName),
+                values: "Text is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.Title),
+                values: "Value is invalid");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.EmailId),
+                values: "Text is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.CountryCode),
+                values: "Text is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.ContactNumber),
+                values: "Text is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.Occupation),
+                values: "Text is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.StudentId),
+                values: "Id is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.CreatedDate),
+                values: "Date is required");
+
+            invalidGuardianException.AddData(
+                key: nameof(Guardian.CreatedBy),
+                values: "Id is required");
+
+            var expectedGuardianValidationException =
+                new GuardianValidationException(invalidGuardianException);
+
+            //when
+            ValueTask<Guardian> addGuardianTask =
+                this.guardianService.AddGuardianAsync(invalidGuardian);
+
+            //then
+            await Assert.ThrowsAsync<GuardianValidationException>(() =>
+                addGuardianTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuardianValidationException))),
+                        Times.Once);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostGuardianAsync(It.IsAny<Guardian>()),
+                    Times.Never);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.apiBrokerMock.VerifyNoOtherCalls();
