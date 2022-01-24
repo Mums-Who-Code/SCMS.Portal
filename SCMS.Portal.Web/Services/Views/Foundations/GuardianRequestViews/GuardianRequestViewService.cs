3eac6908c1fa73db78530f48fd749d6a2dbeb8aa
osp709 <omshriprasath@gmail.com>
2022-01-24 18:59:44 +0530

    ShouldAddGuardianRequestViewAsync -> PASS

--- SCMS.Portal.Web/Services/Views/Foundations/GuardianRequestViews/GuardianRequestViewService.cs
+++ SCMS.Portal.Web/Services/Views/Foundations/GuardianRequestViews/GuardianRequestViewService.cs
@@ -2,9 +2,11 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using SCMS.Portal.Web.Brokers.DateTimes;
 using SCMS.Portal.Web.Brokers.Loggings;
+using SCMS.Portal.Web.Models.Foundations.GuardianRequests;
 using SCMS.Portal.Web.Models.Views.Foundations.GuardianRequestViews;
 using SCMS.Portal.Web.Services.Foundations.GuardianRequests;
 using SCMS.Portal.Web.Services.Foundations.Users;
@@ -30,7 +32,37 @@ namespace SCMS.Portal.Web.Services.Views.Foundations.GuardianRequestViews
             this.loggingBroker = loggingBroker;
         }
 
-        public ValueTask<GuardianRequestView> AddGuardianRequestViewAsync(GuardianRequestView guardianRequestView) =>
-            throw new System.NotImplementedException();
+        public async ValueTask<GuardianRequestView> AddGuardianRequestViewAsync(GuardianRequestView guardianRequestView)
+        {
+            GuardianRequest guardianRequest = MapToGuardianRequest(guardianRequestView);
+            await this.guardianRequestService.AddGuardianRequestAsync(guardianRequest);
+
+            return guardianRequestView;
+        }
+
+        private GuardianRequest MapToGuardianRequest(GuardianRequestView guardianRequestView)
+        {
+            DateTimeOffset currentDateTime = this.dateTimeBroker.GetCurrentDateTime();
+            Guid currentlyLoggedInUserId = this.userService.GetCurrentlyLoggedInUser();
+
+            return new GuardianRequest
+            {
+                Id = Guid.NewGuid(),
+                Title = (GuardianRequestTitle)guardianRequestView.Title,
+                FirstName = guardianRequestView.FirstName,
+                LastName = guardianRequestView.LastName,
+                EmailId = guardianRequestView.EmailId,
+                CountryCode = guardianRequestView.CountryCode,
+                ContactNumber = guardianRequestView.ContactNumber,
+                Occupation = guardianRequestView.Occupation,
+                ContactLevel = (GuardianRequestContactLevel)guardianRequestView.ContactLevel,
+                Relationship = (GuardianRequestRelationship)guardianRequestView.Relationship,
+                StudentId = guardianRequestView.StudentId,
+                CreatedDate = currentDateTime,
+                UpdatedDate = currentDateTime,
+                CreatedBy = currentlyLoggedInUserId,
+                UpdatedBy = currentlyLoggedInUserId
+            };
+        }
     }
 }
