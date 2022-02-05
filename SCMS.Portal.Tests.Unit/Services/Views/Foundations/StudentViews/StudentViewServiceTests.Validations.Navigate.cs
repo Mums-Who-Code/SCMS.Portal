0517f1d6ebf929b665d78779b9d2c63f8eb1b04f
Janane <imjanane@gmail.com>
2022-02-05 15:56:38 -0600

    ShouldThrowValidationExceptionOnNavigateIfRouteIsInvalidAndLogitAsync -> FAIL

---
+++ SCMS.Portal.Tests.Unit/Services/Views/Foundations/StudentViews/StudentViewServiceTests.Validations.Navigate.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using Moq;
+using SCMS.Portal.Web.Models.Views.Foundations.StudentViews.Exceptions;
+using Xunit;
+
+namespace SCMS.Portal.Tests.Unit.Services.Views.Foundations.StudentViews
+{
+    public partial class StudentViewServiceTests
+    {
+        [Theory]
+        [InlineData(data: null)]
+        [InlineData(data: "")]
+        [InlineData(data: " ")]
+        public void ShouldThrowValidationExceptionOnNavigateIfRouteIsInvalidAndLogitAsync(
+            string invalidRoute)
+        {
+            //given
+            var invalidStudentViewException =
+               new InvalidStudentViewException(
+                   parameterName: "Route",
+                   parameterValue: invalidRoute);
+
+            var expectedStudentViewValidationException =
+                new StudentViewValidationException(invalidStudentViewException);
+
+            //when
+            Action navigateToAction = () =>
+                this.studentViewService.NavigateTo(invalidRoute);
+
+            //then
+            Assert.Throws<StudentViewValidationException>(navigateToAction);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentViewValidationException))),
+                        Times.Once);
+
+            this.navigationBrokerMock.Verify(broker =>
+                broker.NavigateTo(It.IsAny<string>()),
+                    Times.Never);
+
+            this.navigationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.userServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.studentServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
