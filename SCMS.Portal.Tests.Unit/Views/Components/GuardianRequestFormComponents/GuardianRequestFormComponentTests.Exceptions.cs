0820a737be0911f1138c07df5f846870e9cd6265
Shri Humrudha <shrija@microsoft.com>
2022-02-05 19:59:16 -0800

    ShouldRenderInnerExceptionMessageIfServiceErrorOccurs -> FAIL

--- SCMS.Portal.Tests.Unit/Views/Components/GuardianRequestFormComponents/GuardianRequestFormComponentTests.Exceptions.cs
+++ SCMS.Portal.Tests.Unit/Views/Components/GuardianRequestFormComponents/GuardianRequestFormComponentTests.Exceptions.cs
@@ -157,5 +157,74 @@ namespace SCMS.Portal.Tests.Unit.Views.Components.GuardianRequestForms
 
             this.guardianRequestViewServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public void ShouldRenderInnerExceptionMessageIfServiceErrorOccurs()
+        {
+            // given
+            Guid randomStudentId = Guid.NewGuid();
+            Guid inputStudentId = randomStudentId;
+            string someMessage = GetRandomString();
+            var serviceExcepton = new Exception(someMessage);
+            string expectedErrorMessage = serviceExcepton.Message;
+
+            ComponentParameter componentParameter = ComponentParameter.CreateParameter(
+                name: nameof(GuardianRequestView.StudentId),
+                value: inputStudentId);
+
+            this.guardianRequestViewServiceMock.Setup(service =>
+                service.AddGuardianRequestViewAsync(It.IsAny<GuardianRequestView>()))
+                    .ThrowsAsync(serviceExcepton);
+
+            // when
+            this.renderedGuardianRequestFormComponent =
+                RenderComponent<GuardianRequestFormComponent>();
+
+            this.renderedGuardianRequestFormComponent.Instance
+                .RegisterButton.Click();
+
+            // then
+            this.renderedGuardianRequestFormComponent.Instance
+                .StatusLabel.Value.Should().Be(expectedErrorMessage);
+
+            this.renderedGuardianRequestFormComponent.Instance
+                .StatusLabel.Color.Should().Be(Color.Red);
+
+            this.renderedGuardianRequestFormComponent.Instance.TitleDropdown.IsDisabled
+                .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.FirstNameTextBox.IsDisabled
+               .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.LastNameTextBox.IsDisabled
+               .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.EmailTextBox.IsDisabled
+               .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.CountryCodeTextBox.IsDisabled
+                .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.ContactNumberTextBox.IsDisabled
+               .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.OccupationTextBox.IsDisabled
+               .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.ContactLevelDropdown.IsDisabled
+               .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.RelationshipDropdown.IsDisabled
+               .Should().BeFalse();
+
+            this.renderedGuardianRequestFormComponent.Instance.RegisterButton.IsDisabled
+               .Should().BeFalse();
+
+            this.guardianRequestViewServiceMock.Verify(service =>
+                service.AddGuardianRequestViewAsync(It.IsAny<GuardianRequestView>()),
+                    Times.Once);
+
+            this.guardianRequestViewServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
