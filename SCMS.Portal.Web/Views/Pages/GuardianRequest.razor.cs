8a300468a752c494f63705cf16c2a815dcb5b663
Shri Humrudha <shrija@microsoft.com>
2022-02-05 16:01:29 -0800

    PAGE: Guardian Request

---
+++ SCMS.Portal.Web/Views/Pages/GuardianRequest.razor.cs
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace SCMS.Portal.Web.Views.Pages
+{
+    public partial class GuardianRequest : ComponentBase
+    {
+        [Parameter]
+        public string StudentId { get; set; }
+    }
+}
