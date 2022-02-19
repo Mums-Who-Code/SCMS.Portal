71c8287ce41d5781bf26a283eb9aa514985dc334
Tharani <tharanisantosh@outlook.com>
2022-02-19 06:07:24 +0200

    ShouldNavigateToRoute -> FAIL

--- SCMS.Portal.Tests.Unit/Services/Views/Foundations/SchoolViews/SchoolViewServiceTests.cs
+++ SCMS.Portal.Tests.Unit/Services/Views/Foundations/SchoolViews/SchoolViewServiceTests.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using Moq;
 using SCMS.Portal.Web.Brokers.Loggings;
+using SCMS.Portal.Web.Brokers.Navigations;
 using SCMS.Portal.Web.Models.Foundations.Schools.Exceptions;
 using SCMS.Portal.Web.Services.Foundations.Schools;
 using SCMS.Portal.Web.Services.Views.Foundations.SchoolViews;
@@ -20,16 +21,19 @@ namespace SCMS.Portal.Tests.Unit.Services.Views.Foundations.SchoolViews
     public partial class SchoolViewServiceTests
     {
         private readonly Mock<ISchoolService> schoolServiceMock;
+        private readonly Mock<INavigationBroker> navigationBrokerMock;
         private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly ISchoolViewService schoolViewService;
 
         public SchoolViewServiceTests()
         {
             this.schoolServiceMock = new Mock<ISchoolService>();
+            this.navigationBrokerMock = new Mock<INavigationBroker>();
             this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
             this.schoolViewService = new SchoolViewService(
                 schoolService: this.schoolServiceMock.Object,
+                navigationBroker: this.navigationBrokerMock.Object,
                 loggingBroker: this.loggingBrokerMock.Object);
         }
 
@@ -51,6 +55,8 @@ namespace SCMS.Portal.Tests.Unit.Services.Views.Foundations.SchoolViews
                 && actualException.InnerException.Message == expectedException.InnerException.Message
                 && (actualException.InnerException as Xeption).DataEquals(expectedException.InnerException.Data);
         }
+
+        private static string GetRandomRoute() => new RandomUrl().GetValue();
 
         private static List<dynamic> CreatedRandomSchoolViewCollections()
         {
--- SCMS.Portal.Web/Services/Views/Foundations/SchoolViews/SchoolViewService.cs
+++ SCMS.Portal.Web/Services/Views/Foundations/SchoolViews/SchoolViewService.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SCMS.Portal.Web.Brokers.Loggings;
+using SCMS.Portal.Web.Brokers.Navigations;
 using SCMS.Portal.Web.Models.Foundations.Schools;
 using SCMS.Portal.Web.Models.Views.Foundations.SchoolViews;
 using SCMS.Portal.Web.Services.Foundations.Schools;
@@ -17,12 +18,15 @@ namespace SCMS.Portal.Web.Services.Views.Foundations.SchoolViews
     {
         private readonly ISchoolService schoolService;
         private readonly ILoggingBroker loggingBroker;
+        private readonly INavigationBroker navigationBroker;
 
         public SchoolViewService(
             ISchoolService schoolService,
+            INavigationBroker navigationBroker,
             ILoggingBroker loggingBroker)
         {
             this.schoolService = schoolService;
+            this.navigationBroker = navigationBroker;
             this.loggingBroker = loggingBroker;
         }
 
@@ -34,6 +38,9 @@ namespace SCMS.Portal.Web.Services.Views.Foundations.SchoolViews
 
             return schools.Select(AsSchoolView).ToList();
         });
+
+        public void NavigateTo(string route) =>
+            throw new NotImplementedException();
 
         private static Func<School, SchoolView> AsSchoolView =>
             school => new SchoolView
