44aaa2983590c63a0ea1576e939f95279e30d609
Tharani <tharanisantosh@outlook.com>
2022-03-04 19:21:16 +0200

    ShouldThrowServiceExceptionOnNavigateIfServiceErrorOccursAndLogIt -> FAIL

---
+++ SCMS.Portal.Tests.Unit/Services/Views/Foundations/SchoolViews/SchoolViewServiceTests.Exceptions.Navigate.cs
@@ -0,0 +1,57 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using SCMS.Portal.Web.Models.Views.Foundations.SchoolViews.Exceptions;
+using Xunit;
+
+namespace SCMS.Portal.Tests.Unit.Services.Views.Foundations.SchoolViews
+{
+    public partial class SchoolViewServiceTests
+    {
+        [Fact]
+        public void ShouldThrowServiceExceptionOnNavigateIfServiceErrorOccursAndLogIt()
+        {
+            // given
+            String someRoute = GetRandomRoute();
+
+            var serviceException = new Exception();
+
+            var failedSchoolViewServiceException =
+                new FailedSchoolViewServiceException(serviceException);
+
+            var expectedSchoolViewServiceException =
+                new SchoolViewServiceException(failedSchoolViewServiceException);
+
+            this.navigationBrokerMock.Setup(service =>
+                service.NavigateTo(It.IsAny<string>()))
+                    .Throws(serviceException);
+
+            // when
+            Action navigateToAction = () =>
+                this.schoolViewService.NavigateTo(someRoute);
+
+            // then
+            Assert.Throws<SchoolViewServiceException>(
+                navigateToAction);
+
+            this.navigationBrokerMock.Verify(service =>
+                service.NavigateTo(It.IsAny<string>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSchoolViewServiceException))),
+                        Times.Once);
+
+            this.navigationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }    
+    }
+}
